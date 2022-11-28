7c5f647d8a386da53700e17bf8c081f3350f82ee
yousaf0811 <yousaf0811@gmail.com>
2022-11-28 21:07:10 +0500

    change class to className

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,23 +3,23 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container">
-            <a class="navbar-brand" href="#">React Users</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <Link class="navbar-brand" href="#">React Users</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" >
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <Link class="nav-link" to="/">Home </Link>
+            <div className="collapse navbar-collapse" >
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/">Home </Link>
                     </li>
                     <li class="nav-item active">
-                        <Link class="nav-link" to="/about">About </Link>
+                        <Link className="nav-link" to="/about">About </Link>
                     </li>
                     <li class="nav-item active">
-                        <Link class="nav-link" to="/contact">Contact US </Link>
+                        <Link className="nav-link" to="/contact">Contact US </Link>
                     </li>
                 </ul>
             </div>
